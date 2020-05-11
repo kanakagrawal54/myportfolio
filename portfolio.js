5bd512aab49f0c4a6fe88e5b4567f0db95f0c057
Kanak Agrawal <kanakagrawal600@gmail.com>
2020-05-11 17:38:42 +0530

    Create portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+function openNavbar(){
+      	document.getElementById("openbtn").style.display="none";
+            document.getElementById("inner-nav-id").style.display="block";
+             	document.getElementById("closebtn").style.display="inline-block";
+            }
+
+        function closeNavbar(){
+        	document.getElementById("openbtn").style.display="inline-block";
+              document.getElementById("inner-nav-id").style.display="none";
+            }
